d88d4d802ba68c22c6e38b4d03b74aaa46aacece
anuraag <anuraag.warudkar@gmail.com>
2024-09-19 04:17:37 -0400

    reduce line count

--- solver.py
+++ solver.py
@@ -1,107 +1,17 @@
 import re
-
 class Solver:
-    
-    def __init__(
-        self,
-        words = '',
-        word_file = 'words.txt',
-        greens: list[str]=['','','','',''], 
-        yellows: dict[str,int]={}, 
-        blacks: str = '', 
-        forbidden_yellow_pos: list[str]=['','','','','']
-    ):
-        
-
-        self.greens = greens
-        self.yellows = yellows
-        self.blacks = blacks
-        self.forbidden_yellow_pos = forbidden_yellow_pos
-        self.solutions: list[str] = []
-        self.words = words
-        with open(file=word_file) as f:
-            self.words += f.read()
-    
-    def gen_pattern(self):  
-        
-        pattern = ''
-        for i in range(5):
-            if self.greens[i] != '':
-                pattern += self.greens[i]
-                continue
-            cur_block = '[^'
-            
-            if self.blacks:
-                cur_block += self.blacks
-            
-            if self.forbidden_yellow_pos[i]:
-                cur_block += self.forbidden_yellow_pos[i]
-            cur_block += ']'
-            pattern += cur_block
-        pattern = fr'^{pattern}$'
-        
-        for char, cnt in self.yellows.items():
-            if cnt == 0:
-                continue
-            if char in self.greens and self.greens.count(char) == cnt:
-                continue
-            
-            cur_lookahead = rf'(?=[a-z]*{rf"{char}[a-z]*"*cnt})'
-            pattern = cur_lookahead + pattern
-            
-        self.pattern = pattern
-        return pattern
-    
-    def search(self, pattern):
-        matches = re.findall(pattern, self.words, re.MULTILINE)
-        if not matches:
-            print("Not found")
-            return None
-        return [''.join(match) for match in matches]
-    
-    def add_guess(self, guess: str, colors: str):
-            self.yellows = {}
-            if len(guess) != 5 or len(colors) != 5:
-                raise ValueError("Guess and colors must be len 5")
-            # Yellow dict will be reset upon each guess
-            for i, (char, color) in enumerate(zip(guess, colors)):
-                match(color):
-                    case'b':
-                        self.blacks += char
-                        continue
-                    case'g':
-                        self.greens[i] = char
-                        self.yellows[char] = self.yellows.get(char,0) + 1
-                        continue
-                    case 'y':
-                        self.forbidden_yellow_pos[i] += char
-                        self.yellows[char] = self.yellows.get(char,0) + 1
-
-            self.solutions = self.search(self.gen_pattern())
-    def get_solutions(self):
-        return self.solutions
+    def __init__(self, word_file='words.txt'): self.words = [line.strip() for line in open(word_file).readlines()]
+    def apply(self, pattern, negate=True): self.words = [word for word in self.words if bool(re.match(pattern, word)) != negate]
+    def guess(self, guess: str, colors: str):
+        for i, (c, color) in enumerate(zip(guess, colors), start=1):
+            match color:
+                case 'b' | 'y': self.apply(f".*{c}.*", negate=color=='b')
+                case 'g': self.apply(f"^{'.'*(i-1)}{c}{'.'*(5-i)}$", negate=False)
+                case 'y': self.apply(f"^{'.'*(i-1)}{c}{'.'*(5-i)}$", negate=True)
 
 solver = Solver(word_file='words.txt')
-
-
-inp = ''
-colors = ''
-for i in range(4):
-
-    while True:
-        inp = input("Input Guess: ")
-        if re.match(r'^[a-z]{5}$', inp) is not None:
-            break
-        if inp == '0':
-            break
-        print("Invalid guess. Must be 5 lowercase letters")
-    guess = inp
-    while True:
-        inp = input("Input Colors: ")
-        if re.match(r'^[gby]{5}$', inp) is not None:
-            break
-        if inp == '0':
-            break
-        print("Invalid colors. Must be 5 instances of [bgy]")
-    solver.add_guess(guess, inp)
-    print(solver.get_solutions())
+for _ in range(4):
+    while not re.match(r'^[a-z]{5}$', (guess := input("Guess: "))): guess = input("Invalid. 5 lowercase: ")
+    while not re.match(r'^[bgy]{5}$', (colors := input("Colors: "))): colors = input("Invalid. 5 [bgy]: ")
+    solver.guess(guess, colors)
+    print(', '.join(solver.words))
